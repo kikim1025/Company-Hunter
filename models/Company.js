e403f76faf58af46916bd6eb90708a4845e7212b
Ki <kikim1025@gmail.com>
2019-07-06 01:20:18 -0400

    finalized api routes and company model

--- models/Company.js
+++ models/Company.js
@@ -1,14 +1,39 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let CompanySchema = new Schema({
+// By default, required validation throws error for 0 length String
+// Enable 0 length String to work
+const isString = function() {
+    return typeof this.desc === 'string' ? '' : 'Company description is required';
+};
+
+// See if all Strings in array match email regex.
+// Additionally check if all emails are unique
+const matchEmail = function(arr) {
+    let arrLen = arr.length;
+    if (arrLen < 4) {
+        let checked = [];
+        for (let i = 0; i < arrLen; i++) {
+            let curr = arr[i];
+            if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(curr) || checked.includes(curr)) {
+                return false;
+            };
+            checked.push(curr);
+        };
+        return true;
+    } else {
+        return false;
+    };
+};
+
+const CompanySchema = new Schema({
     name: {
         type: String,
         trim: true,
         required: 'Company name is required',
         unique: true,
         minlength: 1,
-        maxlength: 50
+        maxlength: 30
     },
     status: {
         type: String,
@@ -18,9 +43,17 @@ let CompanySchema = new Schema({
     desc: {
         type: String,
         trim: true,
+        required: isString,
         maxlength: 2
     },
-
+    contacts: {
+        type: [String],
+        required: 'Company contacts is required',
+        validate: {
+            validator: matchEmail,
+            message: props => `${props.value} is not valid email`
+        }
+    },
     performance: {
         type: Number,
         required: 'Performance score is required',
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,46 +8,97 @@ module.exports = function(app) {
             res.json({ status: 200, data: data, message: 'Company data retrieved successfully' });
         })
         .catch(function (err) {
-            res.json({ status: 500, message: err });
+            res.json({ status: 500, err: err });
         });        
     });
 
     app.post('/api/company', function (req, res) {
         Company.create(req.body)
-        .then(function (data) {
-            res.json({ status: 200, data: data.name, message: 'Company data created successfully' });
+        .then(function () {
+            // Send back updated array
+            Company.find()
+            .then(function (data) {
+                res.json({ status: 200, data: data, message: 'Company data created successfully' });
+            })
+            .catch(function (err) {
+                res.json({ status: 500, err: err });
+            });   
         })
         .catch(function (err) {
-            if (err.name === 'MongoError') { 
+            if (err.name === 'MongoError' && err.code === 11000) { 
                 // Duplicate entry errors are handled here
                 // In case of concurrent create by different users using same company name
-                res.json({ status: 409, message: err });
+                // Refreshes the client's data
+                Company.find()
+                .then(function (data) {
+                    res.json({ status: 409, data: data, message: 'Company already exists, new data sent' });
+                })
+                .catch(function (err) {
+                    res.json({ status: 500, err: err });
+                }); 
+            } else if (err.name === 'ValidationError') { 
+                // If client sends bad request
+                res.json({ status: 400, message: 'Please use the website to manage company data' });     
             } else {
-                res.json({ status: 400, message: 'Please use the website to manage company data' });
-            }
+                res.json({ status: 500, err: err });
+            };
         });
     });
 
     app.put('/api/company', function (req, res) {
-        Company.findOneAndUpdate({ name: req.body.name }, req.body)
-        .then(function (data) {
-            res.json({ status: 200, data: data.name, message: 'Company data updated successfully' });
-        })
-        .catch(function (err) {
-            // Company data nonexistent
-            res.json({ status: 404, message: err });
-        });
+        // Check first if name is included in request, since update search is based on name
+        if (req.body.name) {
+            // Options include enabling Mongoose validation for update
+            Company.findOneAndUpdate({ name: req.body.name }, req.body, { runValidators: true })
+            .then(function (updateData) {
+                Company.find()
+                .then(function (data) {
+                    if (updateData) {
+                        res.json({ status: 200, data: data, message: 'Company data updated successfully' });
+                    } else {
+                        // Company data nonexistent before update req was received
+                        // In case of concurrnt delete by another client
+                        // Sends back refreshed data to client
+                        res.json({ status: 404, data: data, message: 'No such company' });
+                    }
+                })
+                .catch(function (err) {
+                    res.json({ status: 500, err: err });
+                });
+            })
+            .catch(function (err) {
+                if (err.name === 'ValidationError') { // Validation error on properties
+                    res.json({ status: 400, message: 'Please use the website to manage company data' });     
+                } else {
+                    res.json({ status: 500, err: err });
+                };
+            });
+        } else { // Bad request, without the required name property
+            res.json({ status: 400, message: 'Please use the website to manage company data' });
+        };
     });
 
+    // If the requested company data doesn't exist, proceeds as if it was deleted since end result is same
+    // Still, refreshes the client's data
     app.delete('/api/company', function (req, res) {
-        Company.findOneAndDelete({ name: req.body.name })
-        .then(function (data) {
-            res.json({ status: 200, data: data.name, message: 'Company data deleted successfully' });
-        })
-        .catch(function (err) {
-            // Company data nonexistent
-            res.json({ status: 404, message: err });
-        });
+        if (req.body.name && req.body.name.length <= Company.schema.obj.name.maxlength) {
+            Company.findOneAndDelete({ name: req.body.name })
+            .then(function () {
+                Company.find()
+                .then(function (data) {
+                    res.json({ status: 200, data: data, message: 'Company data deleted successfully' });
+                })
+                .catch(function (err) {
+                    res.json({ status: 500, err: err });
+                });  
+            })
+            .catch(function (err) {
+                res.json({ status: 500, err: err });
+            });
+        } else {
+            // Bad request
+            res.json({ status: 400, message: 'Please use the website to manage company data' });
+        };
     });
     
 };
